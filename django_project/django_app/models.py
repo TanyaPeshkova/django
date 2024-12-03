5ca8d1e37d82bc6e3d3640dab8a624c36ea325a7
TanyaPeshkova <peshkova-2002.01@mail.ru>
2024-12-04 00:19:15 +0700

    Добавление API

--- django_project/django_app/models.py
+++ django_project/django_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image_base64 = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
--- django_project/django_app/views.py
+++ django_project/django_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Image
+from .serializers import ImageSerializer
 
-# Create your views here.
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
